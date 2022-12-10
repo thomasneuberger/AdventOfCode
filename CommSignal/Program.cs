f1cea5bfa4de7fbd5cf79ab6d6844c5d56efb6c2
ThomasNeuberger <23504477+thomasneuberger@users.noreply.github.com>
2022-12-10 09:14:51 +0100

    Day 06

---
+++ CommSignal/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Load input data...");
+
+var signal = await File.ReadAllTextAsync("input.txt");
+
+var startPacket = FindStartMarker(signal, 4);
+
+Console.WriteLine($"Start of packet: {startPacket}");
+
+var startMessage = FindStartMarker(signal, 14);
+
+Console.WriteLine($"Start of message: {startMessage}");
+
+int FindStartMarker(string s, int markerLength)
+{
+    for (var i = markerLength - 1; i < s.Length; i++)
+    {
+        var marker = s.Substring(i - markerLength + 1, markerLength);
+        if (marker.Distinct().Count() == markerLength)
+        {
+            return i + 1;
+        }
+    }
+
+    throw new Exception("Start not found.");
+}
