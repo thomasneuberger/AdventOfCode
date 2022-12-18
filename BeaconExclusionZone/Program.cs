318aa229f32e3517f1fd6eaa89fb7ab65f26d285
ThomasNeuberger <23504477+thomasneuberger@users.noreply.github.com>
2022-12-18 19:07:14 +0100

    Day 15

---
+++ BeaconExclusionZone/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using BeaconExclusionZone;
+
+Console.WriteLine("Load input data...");
+
+var input = File.ReadAllLines("input.txt")
+    .Where(l => !l.StartsWith("#"))
+    .ToList();
+    
+var sensors = input
+    .Select(Sensor.Parse)
+    .ToArray();
+
+foreach (var sensor in sensors)
+{
+    Console.WriteLine($"Sensor at {sensor.Position} with beacon {sensor.BeaconAbsolute}, distance: {sensor.BeaconDistance}, X: {sensor.MinX}-{sensor.MaxX}, Y: {sensor.MinY}-{sensor.MaxY}");
+}
+
+var minY = sensors
+    .Select(s => s.Position.X - s.BeaconDistance)
+    .Min();
+var maxY = sensors
+    .Select(s => s.Position.X + s.BeaconDistance)
+    .Max();
+var minX = sensors
+    .Select(s => s.Position.Y - s.BeaconDistance)
+    .Min();
+var maxX = sensors
+    .Select(s => s.Position.Y + s.BeaconDistance)
+    .Max();
+
+var maxCoordinate = 4000000;
+
+var possibleBeacons = FindPossibleBeacons();
+
+foreach (var beacon in possibleBeacons)
+{
+    var tuningFrequency = (beacon.X * 4000000L) + beacon.Y;
+    Console.WriteLine($"Possible beacon at {beacon} with frequency {tuningFrequency}");
+}
+
+IReadOnlyList<Point> FindPossibleBeacons()
+{
+    var beacons = new List<Point>();
+    var timer = new Stopwatch();
+    for (var row = Math.Max(minY, 0); row <= Math.Min(maxY, maxCoordinate); row++)
+    {
+        if (row % 10000 == 0)
+        {
+            Console.WriteLine($"Check row {row} after {timer.Elapsed}...");
+            timer.Restart();
+        }
+        var y = row;
+
+        var possibleSensors = sensors
+            .Where(s => s.MinY <= y)
+            .Where(s => s.MaxY >= y)
+            .ToList();
+
+        var sensorAreas = possibleSensors
+            .Select(s =>
+            {
+                var lineDifference = Math.Abs(y - s.Position.Y);
+                return new
+                {
+                    Left = Math.Max(s.MinX + lineDifference, 0),
+                    Right = Math.Min(s.MaxX - lineDifference, maxCoordinate)
+                };
+            })
+            .OrderBy(s => s.Left);
+
+        var last = -1;
+        foreach (var sensorArea in sensorAreas)
+        {
+            var column = sensorArea.Left - 1;
+            if (sensorArea.Left - 1 > last && sensors.Any(s => !s.WouldDetect(column, y)))
+            {
+                beacons.Add(new Point(column, y));
+            }
+            else
+            {
+                last = Math.Max(last, sensorArea.Right);
+            }
+        }
+    }
+
+    return beacons;
+}
---
+++ BeaconExclusionZone/Sensor.cs
@@ -0,0 +1,81 @@
+﻿using System.Drawing;
+
+namespace BeaconExclusionZone;
+
+public class Sensor
+{
+    private readonly int _positionX;
+    private readonly int _positionY;
+    private readonly int _beaconX;
+    private readonly int _beaconY;
+    private readonly int _minX;
+    private readonly int _maxX;
+    private readonly int _minY;
+    private readonly int _maxY;
+
+    private Sensor(Point position, Point beacon)
+    {
+        Position = position;
+        _positionX = position.X;
+        _positionY = position.Y;
+        BeaconAbsolute = beacon;
+        _beaconX = beacon.X;
+        _beaconY = beacon.Y;
+        BeaconDistance = GetDistance(BeaconAbsolute.X, BeaconAbsolute.Y);
+        _minX = position.X - BeaconDistance;
+        _maxX = position.X + BeaconDistance;
+        _minY = position.Y - BeaconDistance;
+        _maxY = position.Y + BeaconDistance;
+    }
+
+    private int GetDistance(int x, int y)
+    {
+        var distanceX = Math.Abs(_positionX - x);
+        var distanceY = Math.Abs(_positionY - y);
+        return distanceX + distanceY;
+    }
+
+    public Point Position { get; }
+
+    public Point BeaconAbsolute { get; }
+
+    public int BeaconDistance { get; }
+
+    public int MinX => _minX;
+    public int MaxX => _maxX;
+    public int MinY => _minY;
+    public int MaxY => _maxY;
+
+    internal bool WouldDetect(int x, int y)
+    {
+        if (x < _minX || x > _maxX || y < _minY || y > _maxY)
+        {
+            return false;
+        }
+        
+        if ((_beaconX == x && _beaconY == y) || (_positionX == x && _positionY == y))
+        {
+            return true;
+        }
+
+        var distance = GetDistance(x, y);
+        
+        var wouldDetect = distance <= BeaconDistance;
+
+        return wouldDetect;
+    }
+
+    internal static Sensor Parse(string line)
+    {
+        var lineParts = line
+            .Replace("Sensor at ", string.Empty)
+            .Replace(" closest beacon is at ", string.Empty)
+            .Replace("x=", string.Empty)
+            .Replace("y=", string.Empty)
+            .Split(new[] { ":", "," }, StringSplitOptions.TrimEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        return new Sensor(new Point(lineParts[0], lineParts[1]), new Point(lineParts[2], lineParts[3]));
+    }
+}
