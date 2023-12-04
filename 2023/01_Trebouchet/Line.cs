af65d4b58488d9d9cecab394fc1ef6a0895d7416
ThomasNeuberger <23504477+thomasneuberger@users.noreply.github.com>
2023-12-04 14:46:07 +0100

    01/2023

---
+++ 2023/01_Trebouchet/Line.cs
@@ -0,0 +1,67 @@
+﻿namespace _01_Trebouchet;
+
+public class Line(string line, int index)
+{
+    public int Index { get; init; } = index;
+    public string Original { get; init; } = line;
+
+    public string Edited { get; set; } = line;
+
+    public char FirstDigit { get; set; } = '0';
+
+    public char LastDigit { get; set; } = '0';
+
+    public char[] Digits { get; set; } = Array.Empty<char>();
+
+    public string Number { get; set; } = string.Empty;
+
+    public int Value { get; set; } = -1;
+    
+    public char FindFirstDigit((string, int)[] replacements)
+    {
+        for (var i = 0; i < Original.Length; i++)
+        {
+            var charAtPosition = Original[i];
+            if (charAtPosition >= '0' && charAtPosition <= '9')
+            {
+                return charAtPosition;
+            }
+            else
+            {
+                foreach (var replacement in replacements)
+                {
+                    if (Original.Substring(i).StartsWith(replacement.Item1))
+                    {
+                        return replacement.Item2.ToString()[0];
+                    }
+                }
+            }
+        }
+
+        throw new Exception($"First digit in line {Index} not found: {Original}");
+    }
+
+    public char FindLastDigit((string, int)[] replacements)
+    {
+        for (var i = Original.Length - 1; i >= 0; i--)
+        {
+            var charAtPosition = Original[i];
+            if (charAtPosition >= '0' && charAtPosition <= '9')
+            {
+                return charAtPosition;
+            }
+            else
+            {
+                foreach (var replacement in replacements)
+                {
+                    if (Original.Substring(i).StartsWith(replacement.Item1))
+                    {
+                        return replacement.Item2.ToString()[0];
+                    }
+                }
+            }
+        }
+
+        throw new Exception($"Last digit in line {Index} not found: {Original}");
+    }
+}
---
+++ 2023/01_Trebouchet/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Buffers;
+using System.Text.RegularExpressions;
+using _01_Trebouchet;
+
+Part2();
+
+void Part1()
+{
+    var lines = Inputs.Puzzle
+        .Split("\n\r".ToArray(), StringSplitOptions.RemoveEmptyEntries)
+        .Select(l => l.ToArray()
+            .Where(c => c >= '0' && c <= '9')
+            .ToArray())
+        .Select(l => l.First().ToString() + l.Last())
+        .Select(n => int.Parse(n))
+        .ToArray();
+    foreach (var number in lines)
+    {
+        Console.WriteLine(number);
+    }
+
+    Console.WriteLine($"Total: {lines.Sum()}");
+}
+
+
+
+void Part2()
+{
+    var replacements = new[]
+    {
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9)
+    };
+    
+    var lines = Inputs.Puzzle
+        .Split("\n\r".ToArray(), StringSplitOptions.RemoveEmptyEntries)
+        .Select((l, i) => new Line(l, i))
+        .ToList();
+
+    foreach (var line in lines)
+    {
+        line.FirstDigit = line.FindFirstDigit(replacements);
+
+        line.LastDigit = line.FindLastDigit(replacements);
+
+        line.Number = $"{line.FirstDigit}{line.LastDigit}";
+
+        line.Value = int.Parse(line.Number);
+    }
+    foreach (var line in lines)
+    {
+        Console.Write($"Original: {line.Original.PadRight(lines.Max(l => l.Original.Length) + 10)}");
+        Console.Write($"Edited: {line.Edited.PadRight(lines.Max(l => l.Edited.Length) + 8)}");
+        Console.Write($"First digit: {line.FirstDigit.ToString().PadRight(14)}");
+        Console.Write($"Last digit: {line.LastDigit.ToString().PadRight(13)}");
+        Console.Write($"Number: {line.Number.PadRight(lines.Max(l => l.Number.Length) + 8)}");
+        Console.WriteLine($"Value: {line.Value}");
+    }
+
+    Console.WriteLine($"Total: {lines.Sum(l => l.Value)}");
+    
+    // Wrong: 53846 (too low)
+}
+
+
+
