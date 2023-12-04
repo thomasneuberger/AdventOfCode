85dfefe6dc301f7f72c19c7abb2a7453bc27f1b1
ThomasNeuberger <23504477+thomasneuberger@users.noreply.github.com>
2023-12-04 15:20:14 +0100

    02/2023

---
+++ 2023/02_CubeConundrum/Game.cs
@@ -0,0 +1,25 @@
+﻿namespace _02_CubeConundrum;
+
+public class Game
+{
+    public required int Id { get; init; }
+
+    public required string Line { get; set; }
+    
+    public required IReadOnlyList<Draw> Draws { get; init; }
+    
+    public int MaxRed { get; set; }
+    
+    public int MaxBlue { get; set; }
+    
+    public int MaxGreen { get; set; }
+
+    public int Power => MaxRed * MaxBlue * MaxGreen;
+
+    public class Draw
+    {
+        public int Red { get; set; } = 0;
+        public int Blue { get; set; } = 0;
+        public int Green { get; set; } = 0;
+    }
+}
---
+++ 2023/02_CubeConundrum/Program.cs
@@ -0,0 +1,28 @@
+﻿using _02_CubeConundrum;
+
+var games = Inputs.ParseGames(Inputs.Puzzle);
+
+foreach (var game in games)
+{
+    Console.WriteLine(game.Line);
+
+    game.MaxRed = game.Draws.Max(d => d.Red);
+    game.MaxBlue = game.Draws.Max(d => d.Blue);
+    game.MaxGreen = game.Draws.Max(d => d.Green);
+    
+    Console.WriteLine($"Red: {game.MaxRed}, Blue: {game.MaxBlue}, Green: {game.MaxGreen}");
+}
+
+var possibleGames = games
+    .Where(g => g.MaxRed <= 12)
+    .Where(g => g.MaxBlue <= 14)
+    .Where(g => g.MaxGreen <= 13)
+    .ToArray();
+
+foreach (var game in possibleGames)
+{
+    Console.WriteLine($"Possible: {game.Id}, Power: {game.Power}");
+}
+
+Console.WriteLine($"Total: {possibleGames.Sum(g => g.Id)}");
+Console.WriteLine($"Total power: {games.Sum(g => g.Power)}");
