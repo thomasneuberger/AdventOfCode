cd6f15d935979d23c39ae50ac962afe3520488b2
ThomasNeuberger <23504477+thomasneuberger@users.noreply.github.com>
2023-12-17 14:07:53 +0100

    04/2023

---
+++ 2023/04_Scratchcards/Card.cs
@@ -0,0 +1,26 @@
+﻿namespace _04_Scratchcards;
+
+public record class Card
+{
+    public required int Id { get; init; }
+
+    public int Copies { get; set; } = 1;
+
+    public required IDictionary<int, int> WinningNumbers { get; init; }
+
+    public required int[] YourNumbers { get; init; }
+
+    public int? Points { get; set; }
+
+    public override string ToString()
+    {
+        var text = $"Id: {Id}, Winning: {string.Join(", ", WinningNumbers.Keys.Select(n => n.ToString()))}, Your: {string.Join(", ", YourNumbers.Select(n => n.ToString()))}, Copies: {Copies}";
+
+        if (Points.HasValue)
+        {
+            text += $", Points: {Points.Value}";
+        }
+
+        return text;
+    }
+}
---
+++ 2023/04_Scratchcards/Program.cs
@@ -0,0 +1,25 @@
+﻿using _04_Scratchcards;
+
+var cards = Inputs.Parse(Inputs.Puzzle);
+
+for (var index = 0; index < cards.Length; index++)
+{
+    var card = cards[index];
+    var hits = card.YourNumbers.Where(card.WinningNumbers.ContainsKey).Count();
+    card.Points = Math.Max(0, (int)Math.Pow(2, hits - 1));
+    for (int hitIndex = 0; hitIndex < hits; hitIndex++)
+    {
+        var targetIndex = index + hitIndex + 1;
+        if (targetIndex < cards.Length - 1)
+        {
+            cards[targetIndex].Copies += card.Copies;
+        }
+    }
+    Console.WriteLine(card);
+}
+
+var totalPoints = cards.Sum(c => c.Points!.Value);
+var totalCount = cards.Sum(c => c.Copies);
+
+Console.WriteLine($"Total points: {totalPoints}");
+Console.WriteLine($"Total copies: {totalCount}");
